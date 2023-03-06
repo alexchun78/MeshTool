8cc0cfe4ac72c740d86481156acae779be37b81c
alexchun78 <alexchun78@gmail.com>
2023-03-06 19:37:23 +0900

    추가 커밋

---
+++ zLibHalfEdgeRep/zLibHalfEdgeRep.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include <iostream>
+#include <Windows.h>
+#include "include/CDLLManager.h"
+#include "include/CMeshIOManager.h"
+#include "include/CMeshHE.h"
+
+int main()
+{
+    // dll 파일을 로드한다.
+    std::string path = "../Common/dll/zLibMeshIO.dll";
+    CDLLManager* dllManager = new CDLLManager(path.c_str());
+    auto handle = dllManager->GetDLLHandle();
+    if (handle == NULL)
+        return -1;
+
+    CMeshIOManager* ioManager = new CMeshIOManager(handle);
+    bool bRtn = ioManager->LoadSTL("..\\smartslicerA.stl", handle);
+    if (bRtn == false)
+        return -1;
+
+    auto verts = ioManager->GetVertexList();
+    auto tris = ioManager->GetTriangleList();
+
+    MeshHERepLib::CMeshHE* heManager = new MeshHERepLib::CMeshHE(verts, tris);
+
+
+
+    dllManager->TerminateDLL();
+
+    return 0;
+}
+
